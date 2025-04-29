dc118adf94d38b3f7babde3aab25e9ce6201d56a
عبدالله الحديدي <abdallah201@protonmail.com>
2025-04-29 11:06:10 +0300

    Add seeder for Teacher model with user_id association

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Teacher;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -19,5 +20,10 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+        Teacher::create(
+            [
+                'user_id' => 1,
+            ]
+            );
     }
 }
