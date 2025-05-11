9dd1b84bc974735f15ab28c7ad3f56795b6fb0d1
عبدالله الحديدي <abdallah201@protonmail.com>
2025-05-11 16:00:14 +0300

    Add Lesson resource with CRUD pages and migration for lessons table

--- database/migrations/2025_04_30_171722_create_lessons_table.php
+++ database/migrations/2025_04_30_171722_create_lessons_table.php
@@ -4,7 +4,7 @@ use App\Enums\LessonTypeEnum;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use App\Models\{Group, Teacher, Student};
+use App\Models\{Group, Teacher, Student, Surah};
 
 return new class extends Migration
 {
@@ -23,6 +23,35 @@ return new class extends Migration
             $table->date('date');
             $table->time('start_time')->nullable();
             $table->time('end_time')->nullable();
+            $table->string('tajwed_rule')->nullable();
+
+            $table->foreignIdFor(Surah::class, 'read_sora_start_id')->nullable();
+            $table->unsignedSmallInteger('read_aya_start')->nullable();
+            $table->foreignIdFor(Surah::class, 'read_sora_end_id')->nullable();
+            $table->unsignedSmallInteger('read_aya_end')->nullable();
+
+            $table->foreignIdFor(Surah::class, 'hafz_sora_start_id')->nullable();
+            $table->unsignedSmallInteger('hafz_aya_start')->nullable();
+            $table->foreignIdFor(Surah::class, 'hafz_sora_end_id')->nullable();
+            $table->unsignedSmallInteger('hafz_aya_end')->nullable();
+
+            $table->foreignIdFor(Surah::class, 'review_n_sora_start_id')->nullable();
+            $table->unsignedSmallInteger('review_n_aya_start')->nullable();
+            $table->foreignIdFor(Surah::class, 'review_n_sora_end_id')->nullable();
+            $table->unsignedSmallInteger('review_n_aya_end')->nullable();
+
+            $table->foreignIdFor(Surah::class, 'review_f_sora_start_id')->nullable();
+            $table->unsignedSmallInteger('review_f_aya_start')->nullable();
+            $table->foreignIdFor(Surah::class, 'review_f_sora_end_id')->nullable();
+            $table->unsignedSmallInteger('review_f_aya_end')->nullable();
+
+            $table->string('akyda')->nullable();
+            $table->string('hadyth')->nullable();
+            $table->string('fiqha')->nullable();
+            $table->string('akhlak')->nullable();
+
+            $table->decimal('fee', 8, 2)->nullable(); // for reporting/billing
+
             $table->integer('duration_minutes')->nullable(); // for reporting/billing
             $table->timestamps();
         });
