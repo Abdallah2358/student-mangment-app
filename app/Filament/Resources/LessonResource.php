85348755f167429c799b44c6f6ce02e9f6647d53
عبدالله الحديدي <abdallah201@protonmail.com>
2025-08-26 15:59:11 +0300

    Refactor LessonResource form to enhance structure and add advanced options with accordions

--- app/Filament/Resources/LessonResource.php
+++ app/Filament/Resources/LessonResource.php
@@ -8,23 +8,28 @@ use App\Filament\Resources\LessonResource\Pages;
 use App\Filament\Resources\LessonResource\RelationManagers;
 use App\Models\Lesson;
 use App\Models\Teacher;
-use Filament\Forms\Form;
-use Filament\Resources\Resource;
-use Filament\Tables;
-use Filament\Tables\Filters\SelectFilter;
-use Filament\Tables\Table;
-
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\ViewAction;
+use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
-use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\TimePicker;
-use Filament\Forms\Components\Grid;
-use Filament\Forms\Components\Section;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Resources\Resource;
+use Filament\Schemas\Components\Grid;
+use Filament\Schemas\Components\Section;
+use Filament\Schemas\Components\Tabs;
+use Filament\Schemas\Components\Tabs\Tab;
+use Filament\Schemas\Schema;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Filters\Filter;
-use Filament\Schemas\Schema;
-
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
+use LaraZeus\Accordion\Forms\Accordion;
+use LaraZeus\Accordion\Forms\Accordions;
 
 class LessonResource extends Resource
 {
@@ -42,11 +47,11 @@ class LessonResource extends Resource
         return __('lesson.plural');
     }
 
-    public static function form(Schema  $form):Schema 
+    public static function form(Schema $form): Schema
     {
         return $form
             ->schema([
-                Grid::make(2)->schema([
+                Grid::make()->schema([
                     Select::make('teacher_id')
                         ->label(__('index.teacher.title'))
                         ->options(function () {
@@ -58,90 +63,98 @@ class LessonResource extends Resource
                         ->searchable()
                         ->required(),
 
-                    Select::make('group_id')
-                        ->label(__('index.group.title'))
-                        ->relationship('group', 'name')
-                        ->searchable()
-                        ->nullable(),
+                    // Select::make('group_id')
+                    //     ->label(__('index.group.title'))
+                    //     ->relationship('group', 'name')
+                    //     ->searchable()
+                    //     ->nullable(),
 
                     Select::make('student_id')
                         ->label('الطالب')
                         ->relationship('student', 'name')
                         ->searchable()
                         ->nullable(),
-                    Select::make('type')
-                        ->label('نوع الدرس')
-                        ->options(LessonTypeEnum::class)
-                        ->searchable()
-                        ->required(),
+                    // Select::make('type')
+                    //     ->label('نوع الدرس')
+                    //     ->options(LessonTypeEnum::class)
+                    //     ->searchable()
+                    //     ->required(),
 
                     // ->options(collect(LessonTypeEnum::cases())
                     //     ->mapWithKeys(fn(LessonTypeEnum $r) => [$r->value => $r->getLabel()]))
                     // ->required(),
 
 
-                    TextInput::make('topic')->label('الموضوع')->nullable(),
+                    // TextInput::make('topic')->label('الموضوع')->nullable(),
                     DatePicker::make('date')->label('التاريخ')->required(),
                     TimePicker::make('start_time')->label('بداية الدرس')->nullable(),
                     TimePicker::make('end_time')->label('نهاية الدرس')->nullable(),
-                    TextInput::make('tajwed_rule')->label('قاعدة التجويد')->nullable(),
-                ]),
+                    TextInput::make('tajwed_rule')->label('نظري')->nullable(),
+                ])->columnSpanFull(),
+                Accordions::make('')->isolated()
+                    ->activeAccordion(0)
+                    ->columnSpanFull()
+                    ->accordions([
+                        Accordion::make('خيارات متقدمة')
+                            ->schema([
+                                Tabs::make()
+                                    ->tabs(
+                                        [
+                                            Tab::make('القراءة')->schema([
+                                                Select::make('read_sora_start_id')
+                                                    ->label('السورة من')
+                                                    ->options(SurahEnum::class)
+                                                    // ->relationship('readSoraStart', 'name')
+                                                    ->searchable()
+                                                    ->preload()
+                                                    ->nullable(),
+                                                TextInput::make('read_aya_start')->label('الآية من')->numeric()->nullable(),
+                                                Select::make('read_sora_end_id')
+                                                    ->label('السورة إلى')
+                                                    ->options(SurahEnum::class)
+                                                    // ->relationship('readSoraEnd', 'name')
+                                                    ->searchable()
+                                                    ->preload()
+                                                    ->nullable(),
+                                                TextInput::make('read_aya_end')->label('الآية إلى')->numeric()->nullable(),
+                                            ]),
+                                            Tab::make('جديد')
+                                                ->schema([
+                                                    Select::make('hafz_sora_start_id')->label('السورة من')
+                                                        ->relationship('hafzSoraStart', 'name')->searchable()->nullable(),
+                                                    TextInput::make('hafz_aya_start')->label('الآية من')->numeric()->nullable(),
+                                                    Select::make('hafz_sora_end_id')->label('السورة إلى')
+                                                        ->relationship('hafzSoraEnd', 'name')->searchable()->nullable(),
+                                                    TextInput::make('hafz_aya_end')->label('الآية إلى')->numeric()->nullable(),
 
-                Section::make('القراءة')->schema([
-                    Select::make('read_sora_start_id')
-                        ->label('السورة من')
-                        ->options(SurahEnum::class)
-                        // ->relationship('readSoraStart', 'name')
-                        ->searchable()
-                        ->preload()
-                        ->nullable(),
+                                                ]),
+                                            Tab::make('ماضي قريب')->schema([
+                                                Select::make('review_n_sora_start_id')->label('السورة من')
+                                                    ->relationship('reviewNSoraStart', 'name')->searchable()->nullable(),
+                                                TextInput::make('review_n_aya_start')->label('الآية من')->numeric()->nullable(),
+                                                Select::make('review_n_sora_end_id')->label('السورة إلى')
+                                                    ->relationship('reviewNSoraEnd', 'name')->searchable()->nullable(),
+                                                TextInput::make('review_n_aya_end')->label('الآية إلى')->numeric()->nullable(),
+                                            ]),
+                                            Tab::make('ماضي بعيد')->schema([
+                                                Select::make('review_f_sora_start_id')->label('السورة من')
+                                                    ->relationship('reviewFSoraStart', 'name')->searchable()->nullable(),
+                                                TextInput::make('review_f_aya_start')->label('الآية من')->numeric()->nullable(),
+                                                Select::make('review_f_sora_end_id')->label('السورة إلى')
+                                                    ->relationship('reviewFSoraEnd', 'name')->searchable()->nullable(),
+                                                TextInput::make('review_f_aya_end')->label('الآية إلى')->numeric()->nullable(),
+                                            ]),
 
-                    TextInput::make('read_aya_start')->label('الآية من')->numeric()->nullable(),
-
-                    Select::make('read_sora_end_id')
-                        ->label('السورة إلى')
-                        ->options(SurahEnum::class)
-                        // ->relationship('readSoraEnd', 'name')
-                        ->searchable()
-                        ->preload()
-                        ->nullable(),
-
-                    TextInput::make('read_aya_end')->label('الآية إلى')->numeric()->nullable(),
-                ])->columns(2),
-
-                Section::make('الحفظ')->schema([
-                    Select::make('hafz_sora_start_id')->label('السورة من')
-                        ->relationship('hafzSoraStart', 'name')->searchable()->nullable(),
-                    TextInput::make('hafz_aya_start')->label('الآية من')->numeric()->nullable(),
-                    Select::make('hafz_sora_end_id')->label('السورة إلى')
-                        ->relationship('hafzSoraEnd', 'name')->searchable()->nullable(),
-                    TextInput::make('hafz_aya_end')->label('الآية إلى')->numeric()->nullable(),
-                ])->columns(2),
-
-                Section::make('ماضي قريب')->schema([
-                    Select::make('review_n_sora_start_id')->label('السورة من')
-                        ->relationship('reviewNSoraStart', 'name')->searchable()->nullable(),
-                    TextInput::make('review_n_aya_start')->label('الآية من')->numeric()->nullable(),
-                    Select::make('review_n_sora_end_id')->label('السورة إلى')
-                        ->relationship('reviewNSoraEnd', 'name')->searchable()->nullable(),
-                    TextInput::make('review_n_aya_end')->label('الآية إلى')->numeric()->nullable(),
-                ])->columns(2),
-
-                Section::make('ماضي بعيد')->schema([
-                    Select::make('review_f_sora_start_id')->label('السورة من')
-                        ->relationship('reviewFSoraStart', 'name')->searchable()->nullable(),
-                    TextInput::make('review_f_aya_start')->label('الآية من')->numeric()->nullable(),
-                    Select::make('review_f_sora_end_id')->label('السورة إلى')
-                        ->relationship('reviewFSoraEnd', 'name')->searchable()->nullable(),
-                    TextInput::make('review_f_aya_end')->label('الآية إلى')->numeric()->nullable(),
-                ])->columns(2),
-
-                Section::make('مواد إضافية')->schema([
-                    TextInput::make('akyda')->label('العقيدة')->nullable(),
-                    TextInput::make('hadyth')->label('الحديث')->nullable(),
-                    TextInput::make('fiqha')->label('الفقه')->nullable(),
-                    TextInput::make('akhlak')->label('الأخلاق')->nullable(),
-                ])->columns(2),
+                                            Tab::make('مواد إضافية')->schema([
+                                                TextInput::make('akyda')->label('العقيدة')->nullable(),
+                                                TextInput::make('hadyth')->label('الحديث')->nullable(),
+                                                TextInput::make('fiqha')->label('الفقه')->nullable(),
+                                                TextInput::make('akhlak')->label('الأخلاق')->nullable(),
+                                            ]),
+                                        ]
+                                    )->columnSpanFull(),
+                            ]),
+                    ])
             ]);
     }
 
@@ -256,12 +269,12 @@ class LessonResource extends Resource
                 ])
 
             ->actions([
-                Tables\Actions\ViewAction::make(),
-                Tables\Actions\EditAction::make(),
+                ViewAction::make(),
+                EditAction::make(),
             ])
             ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
                 ]),
             ]);
     }
